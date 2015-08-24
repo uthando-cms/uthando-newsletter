1267208bd81407a223f5e3d80187d39ca2998255
shaunfreeman <shaun@shaunfreeman.co.uk>
2015-08-24 21:55:25 +0100

    admin menu update

--- config/module.config.php
+++ config/module.config.php
@@ -114,6 +114,18 @@ return [
     ],
     'router' => [
         'routes' => [
+            'newsletter' => [
+                'type' => 'Segment',
+                'options' => [
+                    'route' => '/newsletter',
+                    'defaults' => [
+                        '__NAMESPACE__' => 'UthandoNewsletter\Controller',
+                        'controller' => 'Preferences',
+                        'action' => 'index',
+                        'force-ssl' => true,
+                    ],
+                ],
+            ],
             'admin' => [
                 'child_routes' => [
                     'newsletter' => [
@@ -291,81 +303,77 @@ return [
     ],
     'navigation' => [
         'admin' => [
-            'modules' => [
+            'newsletter' => [
+                'label' => 'Newsletter',
+                'route' => 'admin/newsletter',
+                'resource' => 'menu:admin',
                 'pages' => [
-                    'newsletter' => [
-                        'label' => 'Newsletter',
-                        'route' => 'admin/newsletter',
+                    'list' => [
+                        'label'     => 'List All Newsletters',
+                        'action'    => 'index',
+                        'route'     => 'admin/newsletter',
+                        'resource'  => 'menu:admin'
+                    ],
+                    'add' => [
+                        'label'     => 'Add New Newsletter',
+                        'action'    => 'add',
+                        'route'     => 'admin/newsletter/edit',
+                        'resource'  => 'menu:admin'
+                    ],
+                    'messages' => [
+                        'label' => 'Messages',
+                        'route' => 'admin/newsletter/message',
                         'resource' => 'menu:admin',
                         'pages' => [
                             'list' => [
-                                'label'     => 'List All Newsletters',
+                                'label'     => 'List All Messages',
                                 'action'    => 'index',
-                                'route'     => 'admin/newsletter',
+                                'route'     => 'admin/newsletter/message',
                                 'resource'  => 'menu:admin'
                             ],
                             'add' => [
-                                'label'     => 'Add New Newsletter',
+                                'label'     => 'Add New Message',
                                 'action'    => 'add',
-                                'route'     => 'admin/newsletter/edit',
+                                'route'     => 'admin/newsletter/message/edit',
                                 'resource'  => 'menu:admin'
                             ],
-                            'messages' => [
-                                'label' => 'Messages',
-                                'route' => 'admin/newsletter/message',
-                                'resource' => 'menu:admin',
-                                'pages' => [
-                                    'list' => [
-                                        'label'     => 'List All Messages',
-                                        'action'    => 'index',
-                                        'route'     => 'admin/newsletter/message',
-                                        'resource'  => 'menu:admin'
-                                    ],
-                                    'add' => [
-                                        'label'     => 'Add New Message',
-                                        'action'    => 'add',
-                                        'route'     => 'admin/newsletter/message/edit',
-                                        'resource'  => 'menu:admin'
-                                    ],
-                                ],
+                        ],
+                    ],
+                    'subscribers' => [
+                        'label' => 'Subscribers',
+                        'route' => 'admin/newsletter/subscriber',
+                        'resource' => 'menu:admin',
+                        'pages' => [
+                            'list' => [
+                                'label'     => 'List All Subscribers',
+                                'action'    => 'index',
+                                'route'     => 'admin/newsletter/subscriber',
+                                'resource'  => 'menu:admin'
                             ],
-                            'subscribers' => [
-                                'label' => 'Subscribers',
-                                'route' => 'admin/newsletter/subscriber',
-                                'resource' => 'menu:admin',
-                                'pages' => [
-                                    'list' => [
-                                        'label'     => 'List All Subscribers',
-                                        'action'    => 'index',
-                                        'route'     => 'admin/newsletter/subscriber',
-                                        'resource'  => 'menu:admin'
-                                    ],
-                                    'add' => [
-                                        'label'     => 'Add New Subscriber',
-                                        'action'    => 'add',
-                                        'route'     => 'admin/newsletter/subscriber/edit',
-                                        'resource'  => 'menu:admin'
-                                    ],
-                                ],
+                            'add' => [
+                                'label'     => 'Add New Subscriber',
+                                'action'    => 'add',
+                                'route'     => 'admin/newsletter/subscriber/edit',
+                                'resource'  => 'menu:admin'
                             ],
-                            'templates' => [
-                                'label' => 'Templates',
-                                'route' => 'admin/newsletter/template',
-                                'resource' => 'menu:admin',
-                                'pages' => [
-                                    'list' => [
-                                        'label'     => 'List All Templates',
-                                        'action'    => 'index',
-                                        'route'     => 'admin/newsletter/template',
-                                        'resource'  => 'menu:admin'
-                                    ],
-                                    'add' => [
-                                        'label'     => 'Add New Template',
-                                        'action'    => 'add',
-                                        'route'     => 'admin/newsletter/template/edit',
-                                        'resource'  => 'menu:admin'
-                                    ],
-                                ],
+                        ],
+                    ],
+                    'templates' => [
+                        'label' => 'Templates',
+                        'route' => 'admin/newsletter/template',
+                        'resource' => 'menu:admin',
+                        'pages' => [
+                            'list' => [
+                                'label'     => 'List All Templates',
+                                'action'    => 'index',
+                                'route'     => 'admin/newsletter/template',
+                                'resource'  => 'menu:admin'
+                            ],
+                            'add' => [
+                                'label'     => 'Add New Template',
+                                'action'    => 'add',
+                                'route'     => 'admin/newsletter/template/edit',
+                                'resource'  => 'menu:admin'
                             ],
                         ],
                     ],
